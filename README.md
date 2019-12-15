9613c87b957de1bab673f45c5e088bccf91d77e7
lovebhatia <javalove007@gmail.com>
2019-12-15 16:10:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-1e4kw3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-1e4kw3)
---
+++ index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+
+
+render(<div><h1>hii</h1><p1>this is my react</p1></div>, document.getElementById('root'));
