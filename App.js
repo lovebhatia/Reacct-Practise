9fd8b423b95f26d2b40ad809a3b2894d47cc7c18
lovebhatia <javalove007@gmail.com>
2019-12-15 18:57:06 +0530

    1 file modified, 3 added, 1 deleted

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Footer from './Footer';
+function App()
+{
+  return(
+     <div>
+     <nav>
+    <h1>Love Bhatia</h1>
+    <p>I am software developer in Newgen Software</p>
+    <ul>
+      <li>India</li>
+      <li>Switzerland</li>
+      <li>Srilanka</li>
+      </ul>
+      </nav>
+      <main>
+      <p>This is where most of my content will go</p>
+      </main>
+      <Footer></Footer>
+    </div>
+  )
+}
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import Hello from './Hello';
 import './style.css';
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import MyInfo from './MyInfo';
+import MyInfo from './components/MyInfo';
 //3.//function Myapp(){return (<ul><li>1</li><li>2</li><li>3</li><li>4</li></ul>);}
 
 //1.//render(<div><h1>hii i am love </h1><p1>this is my react</p1></div>, document.getElementById('root'));
