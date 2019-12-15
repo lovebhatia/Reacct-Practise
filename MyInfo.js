c971e6dba3fdad2bb4c753965ff29646d3e04625
lovebhatia <javalove007@gmail.com>
2019-12-15 18:40:10 +0530

    1 file modified, 1 added

---
+++ MyInfo.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+
+function MyInfo()
+{
+  return (
+    <div>
+    <h1>Love Bhatia</h1>
+    <p>I am software developer in Newgen Software</p>
+    <ul>
+      <li>India</li>
+      <li>Switzerland</li>
+      <li>Srilanka</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MyInfo;
--- index.js
+++ index.js
@@ -2,24 +2,12 @@ import Hello from './Hello';
 import './style.css';
 import React, {Component} from 'react';
 import {render} from 'react-dom';
+import MyInfo from './MyInfo';
 //3.//function Myapp(){return (<ul><li>1</li><li>2</li><li>3</li><li>4</li></ul>);}
 
 //1.//render(<div><h1>hii i am love </h1><p1>this is my react</p1></div>, document.getElementById('root'));
 //2.render(<ul><li>1</li><li>2</li><li>3</li><li>4</li></ul>,
 //document.getElementById('root'));
-function Myinfo()
-{
-  return (
-    <div>
-    <h1>Love Bhatia</h1>
-    <p>I am software developer in Newgen Software</p>
-    <ul>
-      <li>India</li>
-      <li>Switzerland</li>
-      <li>Srilanka</li>
-      </ul>
-    </div>
-  );
-}
-render(<Myinfo/>,document.getElementById('root'));
+
+render(<MyInfo/>,document.getElementById('root'));
 
