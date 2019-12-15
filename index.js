7d6150c34638bdb834d9cb7c6615c3693d252223
lovebhatia <javalove007@gmail.com>
2019-12-15 17:05:41 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+function Myapp()
+{
+  return (
+     <ul>
+     <li>1</li>
+     <li>2</li>
+     <li>3</li>
+     <li>4</li>
+     </ul>
+     );
 
+}
 
+//1.//render(<div><h1>hii i am love </h1><p1>this is my react</p1></div>, document.getElementById('root'));
+//2.render(<ul><li>1</li><li>2</li><li>3</li><li>4</li></ul>,
+//document.getElementById('root'));
+render(<Myapp/>,document.getElementById('root'));
 
-render(<div><h1>hii</h1><p1>this is my react</p1></div>, document.getElementById('root'));
